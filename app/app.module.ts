d4854431e61d8d3432cc8f92db2798991bf51ad3
juanpaprz <54371833+juanpaprz@users.noreply.github.com>
2023-06-27 08:59:09 -0600

    Properties

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,13 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CanvasComponent } from './Components/canvas/canvas.component';
 import { MachineComponent } from './Components/machine/machine.component';
+import { PropertiesComponent } from './Components/properties/properties.component';
 import { SidebarComponent } from './Components/sidebar/sidebar.component';
 import { DataJsonService } from './Services/data-json.service';
 
 @NgModule({
   imports: [BrowserModule, MachineComponent],
   providers: [DataJsonService],
-  declarations: [AppComponent, SidebarComponent, CanvasComponent],
+  declarations: [
+    AppComponent,
+    SidebarComponent,
+    CanvasComponent,
+    PropertiesComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
